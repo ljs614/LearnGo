610128988b3486ca8cf95e29b8807c499fa52065
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-06 11:16:38 +0900

    for, range

--- main.go
+++ main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // func lenAndUpper(name string) (int, string) { //... arg 여러개 받을 수 있음.
 // 	return len(name), strings.ToUpper(name)
 // }
 
-func lenAndUpper(name string) (length int, uppercase string) { //naked return
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
-}
+// func lenAndUpper(name string) (length int, uppercase string) { //naked return
+// 	defer fmt.Println("I'm done")
+// 	length = len(name)
+// 	uppercase = strings.ToUpper(name)
+// 	return
+// }
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// func repeatMe(words ...string) {
+// 	fmt.Println(words)
+// }
+
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers { //변수 하나만 하면 인덱스만 출력.
+		total += number
+	}
+	return total
 }
 
 func main() {
-	len, upperName := lenAndUpper("jeongseop")
-	onlyLen, _ := lenAndUpper("lee") //_ 나머지 무시
-	fmt.Println(len, upperName)
-	fmt.Println(onlyLen)
-
-	repeatMe("nico", "lynn", "dal", "marl")
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
