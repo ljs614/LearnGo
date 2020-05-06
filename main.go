b217e03f0211be3c80548c3483b5f49864209825
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-06 11:24:12 +0900

    if

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // func lenAndUpper(name string) (int, string) { //... arg 여러개 받을 수 있음.
 // 	return len(name), strings.ToUpper(name)
@@ -19,15 +17,23 @@ import (
 // 	fmt.Println(words)
 // }
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers { //변수 하나만 하면 인덱스만 출력.
-		total += number
+// func superAdd(numbers ...int) int {
+// 	total := 0
+// 	for _, number := range numbers { //변수 하나만 하면 인덱스만 출력.
+// 		total += number
+// 	}
+// 	return total
+// }
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
 	}
-	return total
+
+	return true
+
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
