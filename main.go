82bba5670819513e60f7637d4e46dab381652240
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-07 12:25:36 +0900

    Dictionary type

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/ljs614/learngo/accounts"
+	"github.com/ljs614/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("jeongseop")
-	account.Deposit(10000)
-	fmt.Println(account)
-
+	dictionary := mydict.Dictionary{"first": "First word"}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
 }
